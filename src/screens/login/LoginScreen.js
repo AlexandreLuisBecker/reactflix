4a085a85e2276ad6d7ef9c3d7931d6af0ff56fbf
AlexandreLuisBecker <didiao@yahoo.com>
2023-04-03 20:11:19 -0300

    Instalação Biblioteca Icones

---
+++ src/screens/login/LoginScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+// componentes que vao ser utilizados para a tela de login//
+import { 
+    SafeAreaView,
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity
+ } from "react-native";
+
+ // Importando biblioteca de icones // 
+ // npm i react-native-vector-icons//
+
+ import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+ import IonicIcons from 'react-native-vector-icons/Ionicons'
