a175f2d21ac932dfa60e39835234ca29ef584bf5
AlexandreLuisBecker <didiao@yahoo.com>
2023-04-17 21:57:56 -0300

    Criação do esboço da tela inicial

--- App.js
+++ App.js
@@ -3,11 +3,12 @@ import { SafeAreaView,Text } from "react-native";
 
 import LoginScreen from './src/screens/login/LoginScreen'
 import RegisterScreen from './src/screens/register/RegisterScreen'
+import HomeScreen from "./src/screens/home/HomeScreen";
 
 export default function App() {
   return(
     <SafeAreaView>
-    <RegisterScreen/>
+ <HomeScreen></HomeScreen>
 
 
     </SafeAreaView>
